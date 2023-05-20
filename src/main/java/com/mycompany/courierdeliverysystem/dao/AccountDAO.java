1e32afa62968d806e6ca00a92e873637cf0c8d47
Acer <Acer@LAPTOP-EGSS28TV>
2023-05-20 14:44:54 +0800

    Changes

--- src/main/java/com/mycompany/courierdeliverysystem/dao/AccountDAO.java
+++ src/main/java/com/mycompany/courierdeliverysystem/dao/AccountDAO.java
@@ -14,5 +14,10 @@ public class AccountDAO {
     private static final String pass = "admin";
     
     private static final String Add_New_Customer = "INSERT INTO customer (CustId,CustName,Password,CustPhoneNo,email) values (?,?,?,?,?);";
-    private static final String Add_New_Employee = "";
+    private static final String Add_New_Employee = "INSERT INTO employe (EmployeeId,username,password,Name,phoneNo,email,Occupation) values (?,?,?,?,?,?,?)";
+    private static final String View_All_Customer = "SELECT * FROM customer";
+    private static final String View_Customer_By_Id = "SELECT * FROM customer WHERE CustId = ?";
+    private static final String Edit_Customer = "UPDATE customer SET CustName=?, Password=?, CustPhoneNo=?, email=? WHERE CustId=?";
+    private static final String Delete_Customer = "DELETE FROM customer WHERE CustId=?";
+            
 }
